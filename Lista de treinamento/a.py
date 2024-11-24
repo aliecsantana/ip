22e383b5c43990b6945c8fd230049112fbe319e0
Alice Santana <aliecsantana@gmail.com>
2024-11-23 22:12:48 -0300

    Removi algumas linhas de códigos

--- Lista de treinamento/a.py
+++ Lista de treinamento/a.py
@@ -2,17 +2,8 @@ p = int(input())
 
 v = 3
 
-if p < 3:
-    print('O valor precisar ser maior que 3')
-
-else: 
-    divisao = p // v
-    f = p % v
+divisao = p // v
+f = p % v
 
 print(divisao)
 print(f)
-
-
-
-
-
